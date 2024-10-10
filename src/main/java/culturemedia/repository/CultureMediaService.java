c392afe5427a413ef4056226672d94ee45b52bde
FervKa <ferbohi@outlook.com>
2024-10-09 21:59:13 -0500

    Changing addV by add

--- src/main/java/culturemedia/repository/CultureMediaService.java
+++ src/main/java/culturemedia/repository/CultureMediaService.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface CultureMediaService {
     List<Video> listAllVideos();
-    Video addV(Video video);
+    Video add(Video video);
     Video add(View view);
 }
