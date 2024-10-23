cc2a315d6d70c3565b24948fc3c78302fb48b3c8
FervKa <ferbohi@outlook.com>
2024-10-23 18:39:58 -0500

    Adding CultureMediaServicesImpl

---
+++ src/main/java/culturemedia/service/impl/CultureMediaServiceImpl.java
@@ -0,0 +1,24 @@
+package culturemedia.service.impl;
+
+import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.service.CultureMediaService;
+
+import java.util.List;
+
+public class CultureMediaServiceImpl implements CultureMediaService {
+    @Override
+    public List<Video> listAllVideos() {
+        return List.of();
+    }
+
+    @Override
+    public Video addV(Video video) {
+        return null;
+    }
+
+    @Override
+    public Video add(View view) {
+        return null;
+    }
+}
